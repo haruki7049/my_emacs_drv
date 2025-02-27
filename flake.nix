8cebc814b391bdd4c2144416a24a7ddac46179ea
haruki7049 <tontonkirikiri@gmail.com>
2025-02-28 07:34:46 +0900

    Add flakes

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { pkgs, ... }:
+      let
+        emacs = pkgs.callPackage ./. { };
+      in
+      {
+        packages = {
+          inherit emacs;
+          default = emacs;
+        };
+      };
+    };
+}
